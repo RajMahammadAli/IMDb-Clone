85b45b90d24088f553bd2add626dc3e742f86c63
RajMahammadAli <ali.rc.raj@gmail.com>
2024-05-23 15:47:14 +0600

    useSearchParams is covered with suspense

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import NavbarItem from "../navbarItem/NavbarItem";
 
 export default function Navbar() {
   return (
-    <div className="bg-amber-100 dark:bg-gray-400 lg:text-lg p-4 flex justify-center gap-6">
-      <NavbarItem title="Trending" param="fetchtRending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="bg-amber-100 dark:bg-gray-400 lg:text-lg p-4 flex justify-center gap-6">
+        <NavbarItem title="Trending" param="fetchtRending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </div>
+    </Suspense>
   );
 }
